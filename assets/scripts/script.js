0fff10212d37e4796b67642357f623049b126c87
StaticCloud <diegomarrs@gmail.com>
2021-11-10 22:20:03 -0800

    added skeleton functions/flags for password generation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,9 @@
+// set flags for what characters to include in the password
+var lowercase = false;
+var uppercase = false;
+var numeric = false;
+var special = false;
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,8 +13,22 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// primary function that generates password
+var generatePassword = function() {
+  return password;
+}
+
+// prompts the user for the length of the password and returns the value
+var getLength = function() {
+  return length;
+}
+
+// prompts the user for what characters to include in their password
+var getCharacters = function() {
+  
+}
